1ca1e6c65e7d114cd7cc285f6bf87b74a90f44f8
Seongwoo Park <seongwoopark3@gmail.com>
2019-09-21 01:57:00 +0900

    ftp server done

--- B2_ftp_server/run.py
+++ B2_ftp_server/run.py
@@ -6,13 +6,19 @@ from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 
 
+host = "0.0.0.0"
+port = 21
+username = "sentry02"
+password = "teknone!"
+
+
 def main():
     # Instantiate a dummy authorizer for managing 'virtual' users
     authorizer = DummyAuthorizer()
 
     # Define a new user having full r/w permissions and a read-only
     # anonymous user
-    authorizer.add_user("user", "12345", ".", perm="elradfmwMT")
+    authorizer.add_user(username=username, password=password, homedir=".", perm="elradfmwMT")
     authorizer.add_anonymous(os.getcwd())
 
     # Instantiate FTP handler class
@@ -22,14 +28,8 @@ def main():
     # Define a customized banner (string returned when client connects)
     handler.banner = "pyftpdlib based ftpd ready."
 
-    # Specify a masquerade address and the range of ports to use for
-    # passive connections.  Decomment in case you"re behind a NAT.
-    #handler.masquerade_address = "151.25.42.11"
-    #handler.passive_ports = range(60000, 65535)
-
-    # Instantiate FTP server class and listen on 0.0.0.0:2121
-    address = ("0.0.0.0", 2121)
-    server = FTPServer(address, handler)
+    # Instantiate FTP server class and listen on host:port
+    server = FTPServer((host, port), handler)
 
     # set a limit for connections
     server.max_cons = 256
